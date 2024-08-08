1d3587fc4056660e8d7ca07f594d06b57ad2e270
sambeep <samsonkinyanjui11@gmail.com>
2024-08-08 23:39:44 +0800

    update tests

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-
-from flask import Flask
-from models import storage
+"""
+Module to register blueprints and run the flask server
+in preparation for api calls
+"""
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import engine
+from models import storage
+from os import getenv
 
-def create_app():
-    app = Flask(__name__)
-    
-    # Register the blueprint
-    app.register_blueprint(app_views)
-    
-    # Teardown context
-    @app.teardown_appcontext
-    def teardown(exception):
-        storage.close()
-    
-    return app
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-app = create_app()
+
+@app.teardown_appcontext
+def teardown_flask(exception):
+    """
+    Remove the database, exit and save file
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return (jsonify(error="Not found"), 404)
 
 if __name__ == "__main__":
-    from os import environ
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port)
